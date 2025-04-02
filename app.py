a33b4ada273e96bc77dc4bc5cd71aed397b652b7
sharmaaakash170 <sharmaaakash170@gmail.com>
2025-04-02 11:51:38 +0530

    Adding route

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello, DevOps World!!! From DevOps Engineer Aakash"
+    return "Hello from Flask!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/hello")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
