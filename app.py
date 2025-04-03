d48aeb57a1caa48d6ef8e17819e378e065725bce
sharmaaakash170 <sharmaaakash170@gmail.com>
2025-04-03 12:44:28 +0530

    update

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask!"
+    return "Hello from High person!"
 
 @app.route("/hello")
 def hello():
-    return "Hello, World!"
+    return "Hey aakash, he is high"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
